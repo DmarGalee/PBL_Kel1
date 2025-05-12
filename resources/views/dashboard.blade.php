9050a43e38983362ecd7cca74fe4c3416be47677
aslamrosul <nvrsrndrmrx@gmail.com>
2025-05-12 23:32:59 +0700

    Semua anggota - yajra-datatables, laravel-dompdf, laravel-spreadsheet, auth,dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,44 +6,6 @@
             <!-- Small boxes (Stat box) -->
             <div class="row">
                
-                <div class="col-lg-3 col-6">
-                    <!-- small box -->
-                    <div class="small-box bg-success">
-                        <div class="inner">
-                            <h3>1</h3>
-                            <p>Total Pendapatan</p>
-                        </div>
-                        <div class="icon">
-                            <i class="ion ion-stats-bars"></i>
-                        </div>
-                    </div>
-                </div>
-                <!-- ./col -->
-                <div class="col-lg-3 col-6">
-                    <!-- small box -->
-                    <div class="small-box bg-warning">
-                        <div class="inner">
-                            <h3>1</h3>
-                            <p>Total Barang</p>
-                        </div>
-                        <div class="icon">
-                            <i class="ion ion-cube"></i>
-                        </div>
-                    </div>
-                </div>
-                <!-- ./col -->
-                <div class="col-lg-3 col-6">
-                    <!-- small box -->
-                    <div class="small-box bg-danger">
-                        <div class="inner">
-                            <h3>1</h3>
-                            <p>Total Kategori</p>
-                        </div>
-                        <div class="icon">
-                            <i class="ion ion-pie-graph"></i>
-                        </div>
-                    </div>
-                </div>
                 <!-- ./col -->
                 <div class="col-lg-3 col-6">
                     <!-- small box -->
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
+
+
+Route::pattern('id', '[0-9]+'); //jika ada parameter id, maka harus berupa angka
+// register
+Route::get('register', [AuthController::class, 'register'])->name('register');
+Route::post('register', [AuthController::class, 'postRegister']);
+// login
+Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::post('login', [AuthController::class, 'postlogin']);
+//logout
+Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
+
+
+
+Route::middleware(['auth'])->group(function () { //artinya semua route di dalam goup ini harus login dulu
+    // masukkan semua route yang perlu autentikasi di sini
+
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 });
