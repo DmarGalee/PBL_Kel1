50fa5a42bb46c1a693d4cb45f42d2404ce1ee168
aslamrosul <nvrsrndrmrx@gmail.com>
2025-05-13 07:49:01 +0700

    semua anggota - migration lantai

---
+++ database/migrations/2025_05_13_004621_create_m_lantai_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('m_lantai', function (Blueprint $table) {
+            $table->id('lantai_id');
+            $table->string('lantai_nomor'); // Contoh: "Lantai 1", "B1", dst
+            $table->text('deskripsi')->nullable(); // Opsional
+            $table->foreignId('gedung_id')->constrained('m_gedung')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('m_lantai');
+    }
+};
